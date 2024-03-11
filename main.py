85d17e814fa64d9a909021436c3da8917a4f61bb
Kainoa2 <160158955+Kainoa2@users.noreply.github.com>
2024-03-11 11:53:44 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-#This file was created by: Keawe Ainoa
+# This file was created by: Keawe Ainoa 
+
 # import libraries and modules
+
+
 import pygame as pg
 from settings import *
 from sprites import *
-from random import randint
+import random
 import sys
 from os import path
+
+HEALTH = 10
 
 # Define game class...
 class Game:
@@ -22,16 +27,6 @@ class Game:
     def load_data(self):
         game_folder = path.dirname(__file__)
         self.map_data = []
-        # 'r'     open for reading (default)
-        # 'w'     open for writing, truncating the file first
-        # 'x'     open for exclusive creation, failing if the file already exists
-        # 'a'     open for writing, appending to the end of the file if it exists
-        # 'b'     binary mode
-        # 't'     text mode (default)
-        # '+'     open a disk file for updating (reading and writing)
-        # 'U'     universal newlines mode (deprecated)
-        # below opens file for reading in text mode
-        # with 
         '''
         The with statement is a context manager in Python. 
         It is used to ensure that a resource is properly closed or released 
@@ -41,21 +36,35 @@ class Game:
             for line in f:
                 print(line)
                 self.map_data.append(line)
+
     # Create run method which runs the whole GAME
     def new(self):
         print("create new game...")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player1 = Player(self, 1, 1)
+        self.coins = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.power_ups = pg.sprite.Group()
+        # self.player1 = Player(self, 1, 1)
         # for x in range(10, 20):
         #     Wall(self, x, 5)
         for row, tiles in enumerate(self.map_data):
             print(row)
             for col, tile in enumerate(tiles):
                 print(col)
-                if tile == 'w':
+                if tile == '1':
                     print("a wall at", row, col)
                     Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)   
+                if tile == 'M':
+                    Mob(self, col, row)
+                if tile == 'U':
+                    PowerUp(self, col, row)
+                if tile != '1':
+                    number = random.randint(1,150)
+                    if number == 4:
+                        Coin(self, col, row)
 
 
 
@@ -79,12 +88,29 @@ class Game:
               pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
          for y in range(0, HEIGHT, TILESIZE):
               pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
-
+    
+    def draw_text(self, surface, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (x,y)
+        surface.blit(text_surface, text_rect)
+    
     def draw(self):
+            global HEALTH
+            keys = pg.key.get_pressed()
+            if keys[pg.K_1]:
+                HEALTH -= 1
             self.screen.fill(BGCOLOR)
             self.draw_grid()
             self.all_sprites.draw(self.screen)
+            self.draw_text(self.screen, "Coins 2" + str(self.player.moneybag), 24, WHITE, WIDTH/2 - 32, 2)
+            pg.draw.rect(self.screen, LIGHTGREY, pg.Rect(0,0,320,32))
+            pg.draw.rect(self.screen, RED, pg.Rect(0,0,HEALTH *32,32))
+            
             pg.display.flip()
+    
 
     def events(self):
          for event in pg.event.get():
@@ -92,21 +118,38 @@ class Game:
                 self.quit()
             # if event.type == pg.KEYDOWN:
             #     if event.key == pg.K_LEFT:
-            #         self.player1.move(dx=-1)
+            #         self.player.move(dx=-1)
             #     if event.key == pg.K_RIGHT:
-            #         self.player1.move(dx=1)
+            #         self.player.move(dx=1)
             #     if event.key == pg.K_UP:
-            #         self.player1.move(dy=-1)
+            #         self.player.move(dy=-1)
             #     if event.key == pg.K_DOWN:
-            #         self.player1.move(dy=1)
-            
-            
+            #         self.player.move(dy=1)
+                
+
+    def show_start_screen(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_text(self.screen, "This is the start screen - press any key to play", 24, WHITE, WIDTH/2 - 200, HEIGHT/2 - 24)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.quit()
+                if event.type == pg.KEYUP:
+                    waiting = False
 
 # Instantiate the game... 
 g = Game()
 # use game method run to run
-# g.show_start_screen()
+g.show_start_screen()
 while True:
     g.new()
     g.run()
     # g.show_go_screen()
+    print (HEALTH)
--- settings.py
+++ settings.py
@@ -8,10 +8,16 @@ TITLE = "My Fun Game"
 TILESIZE = 32
 
 
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 BGCOLOR = (0,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+RED = (255,0,0)
+PURPLE = (255,0,255)
+YELLOW = (255,255,0)
 LIGHTGREY = (75,75,75)
+DARKGRAY = (200,200,200)
 
-# Player attributes
-PLAYER_SPEED = 200
+# Player settings
+PLAYER_SPEED = 300
